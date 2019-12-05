62cc402930090902cf4ef4087bb884d1f390bd39
mateus <mateusnt88@gmail.com>
2019-12-04 23:22:35 -0300

    Imagem de Fundo

--- lib/main.dart
+++ lib/main.dart
@@ -32,6 +32,7 @@ class _HomeState extends State<Home> {
    String _2p = "";
    String _3p = "";
    String _4p = "";
+   String anos = "";
 
 void _abc(String value){
   setState((){
@@ -70,44 +71,31 @@ void _apagarTudo(){
 
 void mostrarTela(){
   setState((){
-    _nome = ("Nome da Criança: ${controllerNome.text}");
-    _sx = ("Sexo da Criança: ${_sexo}");
-    _responsavel = ("Nome do Responsável: ${controllerResp.text}");
-    _entrada = ("Hora de Entrada: ${controllerHora.text}");
-    _id = ("Idade da criaça: ${_idadeDT} anos");
-    _1p = "...";
-    _2p = "...";
-    _3p = "...";
-    _4p = "...";
+    if (_idadeDT == "3" || _idadeDT == "2"){
+      anos = "Anos";
+    }else if (_idadeDT == "1"){
+      anos = "Ano";
+    }else if(_idadeDT == "0.1"){
+      anos = "Mês";
+    }
+    else{
+      anos = "Meses";
+    }
+    _nome = ("Nome da Criança:\n ${controllerNome.text}");
+    _sx = ("Sexo da Criança:\n ${_sexo}");
+    _responsavel = ("Nome do Responsável:\n ${controllerResp.text}");
+    _entrada = ("Hora de Entrada:\n ${controllerHora.text}");
+    _id = ("Idade da criança:\n ${_idadeDT} ${anos}");
+    _1p = "_____________________________________________________";
+    _2p = "_____________________________________________________";
+    _3p = "_____________________________________________________";
+    _4p = "_____________________________________________________";
     
     
 
 
-  });
-  // AlertDialog alertDialog = AlertDialog(
-  //   content: Container(
-  //     height: 200.0,
-  //     child: Column(
-  //       children: <Widget>[
-  //         Text("Nome : ${controllerNome.text}"),
-  //         Text("Sexo da Criança : ${_sexo}"),
-  //         Text("Responsavel : ${controllerResp.text}"),
-  //         Text("Hora da Entrada : ${controllerHora.text}"),
-  //         Text("Idade : ${_idadeDT} anos"),
-  //         RaisedButton(
-  //           child: Text("Voltar"),
-  //           onPressed: ()=> Navigator.pop(context),
-  //         ),
-  //       ],
-  //     ),
-  //   ),
-  // );
-  // showDialog(context: context, child: alertDialog);
-  
+  });  
 }
-  //GlobalKey<FormState>_key = GlobalKey();
-  //bool _validate = false;
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -124,10 +112,16 @@ void mostrarTela(){
        ),
        backgroundColor: Colors.white,
         //singleChild
-      body: 
-      SingleChildScrollView( 
+      body: Stack(
+        children: <Widget>[
+          Image.asset("imagens/carinhosos.jpg",
+          fit: BoxFit.cover,
+          height: 500.0,
+          width: 600.0, 
+          ),
+        SingleChildScrollView( 
         //children: <Widget>[
-            padding: EdgeInsets.all(10.0),
+          padding: EdgeInsets.all(00.0),
           child: Form(  
             key: _formKey,
 
@@ -136,8 +130,8 @@ void mostrarTela(){
               children: <Widget>[
 
                 Text("          Bem vindo ao nosso Berçario!!! \n Por favor Preencha nosso formulario Digital", 
-                style: TextStyle(fontSize:17.0),),
-                Padding(padding: EdgeInsets.only(top: 20.0),),
+                style: TextStyle(fontSize:17.5,fontWeight: FontWeight.bold,),),
+                Padding(padding: EdgeInsets.only(top: 25.0),),
 
                 TextFormField(
                   keyboardType: TextInputType.text,
@@ -147,30 +141,30 @@ void mostrarTela(){
                     }
                   },
                   controller: controllerNome,
+                  style: TextStyle(fontSize:22.0,fontWeight: FontWeight.bold,color: Colors.black),                    
+
                   decoration: InputDecoration(
-                    
                     hintText: "Nome Completo",
                     labelText: "Nome Completo da Criança",
+                    labelStyle: TextStyle(color: Colors.black),
+                    hintStyle: TextStyle(color: Colors.black,),
+
                     border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(20.0)
                     )
                   ),
                 ),
                 Padding(padding: EdgeInsets.only(top: 20.0),),
-
+                
                   RadioListTile(
-                  // validator : (value){
-                  //   if (value == null){
-                  //     return "Informe seu sexo";
-                  //   }
-                  // },
+                  
                   value: "Masculino",
                   title: Text("Masculino"),
                   groupValue: _sexo,
                   onChanged: (String value){
                     _abc(value);
                   },
-                  activeColor: Colors.red,
+                  activeColor: Colors.blue,
                 ),
 
                   RadioListTile(
@@ -180,7 +174,7 @@ void mostrarTela(){
                   onChanged: (String value){
                     _abc(value);
                   },
-                  activeColor: Colors.red,
+                  activeColor: Colors.blue,
                 ),
 
                 Padding(padding: EdgeInsets.only(top: 20.0),),
@@ -192,9 +186,13 @@ void mostrarTela(){
                     }
                   },
                   controller: controllerResp,
+                  style: TextStyle(fontSize:22.0,fontWeight: FontWeight.bold,color: Colors.black),                    
+
                   decoration: InputDecoration(
                     hintText: "Nome do Responsável",
                     labelText: "Nome do Responsável",
+                    labelStyle: TextStyle(color: Colors.black),
+                    hintStyle: TextStyle(color: Colors.black,),
                     border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(20.0))
                 ),
@@ -209,9 +207,13 @@ void mostrarTela(){
                     }
                   },
                   controller : controllerHora,
+                  style: TextStyle(fontSize:22.0,fontWeight: FontWeight.bold,color: Colors.black),                    
+
                   decoration: InputDecoration(
                     hintText: "Hora da Entrada",
                     labelText: "Hora da Entrada",
+                    labelStyle: TextStyle(color: Colors.black),
+                    hintStyle: TextStyle(color: Colors.black,),
                     border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(20.0)
                     )
@@ -221,7 +223,7 @@ void mostrarTela(){
 
                 Row(
                   children: <Widget>[
-                    Text("Idade   ", style: TextStyle(fontSize:20.0,color:Colors.black),),
+                    Text("Idade   ", style: TextStyle(fontSize:22.0,color:Colors.black, fontWeight: FontWeight.bold),),
                   DropdownButton(
                     
                   onChanged: (String value){
@@ -252,33 +254,33 @@ void mostrarTela(){
               Text(
                  _nome ,
                  textAlign: TextAlign.center,
-                  style: TextStyle(color: Colors.black,fontSize: 20.0),
+                  style: TextStyle(color: Colors.black,fontSize: 22.0, fontWeight: FontWeight.bold,),
                 ),
                 Text(_1p,),
 
                 Text(_sx,
                 textAlign: TextAlign.center,
-                style: TextStyle(color: Colors.black,fontSize: 20.0),
+                  style: TextStyle(color: Colors.black,fontSize: 22.0, fontWeight: FontWeight.bold,),
                 ),
                 
                 Text(_2p,),
 
                 Text(_responsavel,
                 textAlign: TextAlign.center,
-                style: TextStyle(color: Colors.black,fontSize: 20.0),
+                  style: TextStyle(color: Colors.black,fontSize: 22.0, fontWeight: FontWeight.bold,),
                 ),
                 Text(_3p,),
 
                 Text(_entrada,
                 textAlign: TextAlign.center,
-                style: TextStyle(color: Colors.black,fontSize: 20.0),
+                  style: TextStyle(color: Colors.black,fontSize: 22.0, fontWeight: FontWeight.bold,),
                 ),
 
                 Text(_4p,),
 
                 Text(_id,
                 textAlign: TextAlign.center,
-                style: TextStyle(color: Colors.black,fontSize: 20.0),
+                  style: TextStyle(color: Colors.black,fontSize: 22.0, fontWeight: FontWeight.bold,),
                 ),
               // Text("Sexo da Criança : ${_sexo}"),
               // Text("Responsavel : ${controllerResp.text}"),
@@ -291,6 +293,8 @@ void mostrarTela(){
 
         //],
       ),
+       ],
+      )
     );
   } 
 }
